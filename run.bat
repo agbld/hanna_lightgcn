a19165aa34ed4435d0daaf0435325cfd8a1e6d9f
agiblida <agiblida@gmail.com>
2024-02-29 11:50:02 +0800

    init

---
+++ run.bat
@@ -0,0 +1,28 @@
+echo 1/14
+python main.py 2018-12-31_18 --embed_size 1024 --n_layers 1 --batch_size 5000 --epochs 10 --learning_rate 2e-05 --decay 0
+echo 2/14
+python main.py 2019-01-31_18 --embed_size 1024 --n_layers 1 --batch_size 5000 --epochs 10 --learning_rate 2e-05 --decay 0
+echo 3/14
+python main.py 2019-02-28_18 --embed_size 1024 --n_layers 1 --batch_size 5000 --epochs 10 --learning_rate 2e-05 --decay 0
+echo 4/14
+python main.py 2019-03-31_18 --embed_size 1024 --n_layers 1 --batch_size 5000 --epochs 10 --learning_rate 2e-05 --decay 0
+echo 5/14
+python main.py 2019-04-30_18 --embed_size 1024 --n_layers 1 --batch_size 5000 --epochs 10 --learning_rate 2e-05 --decay 0
+echo 6/14
+python main.py 2019-05-31_18 --embed_size 1024 --n_layers 1 --batch_size 5000 --epochs 10 --learning_rate 2e-05 --decay 0
+echo 7/14
+python main.py 2019-06-30_18 --embed_size 1024 --n_layers 1 --batch_size 5000 --epochs 10 --learning_rate 2e-05 --decay 0
+echo 8/14
+python main.py 2018-12-31_6 --embed_size 1024 --n_layers 1 --batch_size 5000 --epochs 10 --learning_rate 2e-05 --decay 0
+echo 9/14
+python main.py 2019-01-31_6 --embed_size 1024 --n_layers 1 --batch_size 5000 --epochs 10 --learning_rate 2e-05 --decay 0
+echo 10/14
+python main.py 2019-02-28_6 --embed_size 1024 --n_layers 1 --batch_size 5000 --epochs 10 --learning_rate 2e-05 --decay 0
+echo 11/14
+python main.py 2019-03-31_6 --embed_size 1024 --n_layers 1 --batch_size 5000 --epochs 10 --learning_rate 2e-05 --decay 0
+echo 12/14
+python main.py 2019-04-30_6 --embed_size 1024 --n_layers 1 --batch_size 5000 --epochs 10 --learning_rate 2e-05 --decay 0
+echo 13/14
+python main.py 2019-05-31_6 --embed_size 1024 --n_layers 1 --batch_size 5000 --epochs 10 --learning_rate 2e-05 --decay 0
+echo 14/14
+python main.py 2019-06-30_6 --embed_size 1024 --n_layers 1 --batch_size 5000 --epochs 10 --learning_rate 2e-05 --decay 0
